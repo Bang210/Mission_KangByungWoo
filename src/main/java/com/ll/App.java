8592d91198808973830a2ecf27f20a905f665bbc
Bang210 <bangvelope@gmail.com>
2023-11-06 10:17:42 +0900

    종료 기능, Rq class 구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,20 @@
 package com.ll;
 
+import java.util.Scanner;
+
 class App {
+    Scanner scanner = new Scanner(System.in);
     public void run() {
+        //초기 메세지
         System.out.println("==명언 앱==");
-        System.out.print("명령) ");
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+            Rq rq = new Rq(cmd);
+            switch (rq.getAction()) {
+                case "종료" :
+                    return;
+            }
+        }
     }
 }
---
+++ src/main/java/com/ll/Rq.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+class Rq {
+    String cmd;
+    Rq(String cmd) {
+        this.cmd = cmd;
+    }
+    String getAction() {
+        return cmd;
+    }
+}
